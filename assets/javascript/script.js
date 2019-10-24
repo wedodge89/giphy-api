8b51142f96930732a00e44f5575e34f4448ca9d0
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2019-10-24 18:24:02 -0400

    added button-add functionality, enabled clicking for user generated buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,21 +2,55 @@ $(document).ready(function() {
 
     let topics = ["My Hero Academia", "Attack on Titan", "Demon Slayer", "Yu Yu Hakusho", "Code Geass"]
 
-    function printButtons() {
+    function makeButtons() {
         $("#buttonDiv").empty();
         for (let i = 0; i < topics.length; i ++) {
-            var a = $("<button>");
-            a.attr("value", topics[i]);
-            a.text(topics[i]);
-            a.attr("class", "gifButton");
-            $("#buttonDiv").append(a);
+            var btn = $("<button>");
+            btn.attr("value", topics[i]);
+            btn.text(topics[i]);
+            btn.attr("class", "gifButton");
+            $("#buttonDiv").append(btn);
         }
     }
 
-    printButtons();
+    makeButtons();
 
-    $(".gifButton").on("click", function() {
-        let query = $(this).val()
-        console.log(query)
+    $(document.body).on("click", ".gifButton", function() {
+        let query = $(this).val();
+        const queryURL = "https://api.giphy.com/v1/gifs/search?api_key=yQZnEQW0eMlyNZ4WYRZhN5QZcREtbMTp&q=" + query + "&limit=10&offset=0&rating=G&lang=en"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            let results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                let gifDiv = $("<div>");
+                let rating = results[i].rating;
+                let p = $("<p>").text(`Rating: ${rating}`);
+                let animeGif = $("<img>");
+                animeGif.attr("src", results[i].images.fixed_height_still.url);
+                animeGif.attr("data-state", "still")
+                gifDiv.prepend(p);
+                gifDiv.prepend(animeGif);
+                $("#gifDivDiv").prepend(gifDiv);
+            }});
+        })
+
+    $("#submit-button").on("click", function(event) {
+        event.preventDefault();
+        let showName=$("#show-input").val().trim();
+        topics.push(showName);
+        console.log(topics);
+        makeButtons();
+        $("#giphy-form input[type='text']").val("");
+    })
+
+    $(document).on("click", ".clickableGif", function() {
+        state = $(this).attr("data-state")
+        if (state = "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate")
+        }
     })
 });
